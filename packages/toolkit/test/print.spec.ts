ff3ce0bd5af2631e5314fcad107e72600a9385e1
why520crazy <why520crazy@163.com>
2020-04-13 09:20:29 +0800

    feat(toolkit): export chalk and colors from Print

--- packages/toolkit/test/print.spec.ts
+++ packages/toolkit/test/print.spec.ts
@@ -6,11 +6,12 @@ import * as TypeMoq from 'typemoq';
 describe('#toolkit.print', () => {
     beforeEach(() => {});
 
-    it('should been plural when word is books ', () => {
-        const mock: TypeMoq.IMock<Console> = TypeMoq.Mock.ofInstance<Console>(console, TypeMoq.MockBehavior.Strict, true);
-        toolkit.print.debug('debug');
-        mock.reset();
-        // toolkit.print.nativeInfo('books', 'sdd');
+    it('should print info', () => {
+        // const mock: TypeMoq.IMock<Console> = TypeMoq.Mock.ofInstance<Console>(console, TypeMoq.MockBehavior.Strict, true);
+        // toolkit.print.debug('debug');
+        // mock.reset();
+
+        toolkit.print.fancy('books', 'sdd');
         // toolkit.print.info('info', 'info2', { name: 2 });
         // toolkit.print.warn('warn');
         // toolkit.print.debug('debug');
