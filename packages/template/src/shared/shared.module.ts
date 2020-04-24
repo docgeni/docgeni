668adf47df5a99432ba4477ce57f9689e6e1cd0f
why520crazy <why520crazy@163.com>
2020-04-24 11:44:55 +0800

    feat(template): add content-view component #INFR-374

--- packages/template/src/shared/shared.module.ts
+++ packages/template/src/shared/shared.module.ts
@@ -9,9 +9,10 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { ExampleViewerComponent } from './example-viewer/example-viewer.component';
 import { IconComponent } from './icon/icon.component';
+import { ContentViewerComponent } from './content-viewer/content-viewer.component';
 
 @NgModule({
-    declarations: [NavbarComponent, FooterComponent, SidebarComponent, ExampleViewerComponent, IconComponent],
+    declarations: [NavbarComponent, FooterComponent, SidebarComponent, ExampleViewerComponent, IconComponent, ContentViewerComponent],
     imports: [CommonModule, BrowserModule, FormsModule, RouterModule, HttpClientModule],
     exports: [
         CommonModule,
@@ -23,9 +24,10 @@ import { IconComponent } from './icon/icon.component';
         SidebarComponent,
         FooterComponent,
         ExampleViewerComponent,
+        ContentViewerComponent,
         IconComponent
     ]
 })
 export class DocgeniSharedModule {}
 
-export { NavbarComponent, FooterComponent, SidebarComponent, ExampleViewerComponent, IconComponent };
+export { NavbarComponent, FooterComponent, SidebarComponent, ExampleViewerComponent, IconComponent, ContentViewerComponent };
