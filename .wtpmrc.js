b7eb142dd2732a0827da2661a9fac7e107e839bb
why520crazy <why520crazy@163.com>
2020-05-27 13:16:23 +0800

    build: release and pub use wpm

---
+++ .wtpmrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    defaultBranch: 'master',
+    bumpFiles: [
+        'package.json',
+        {
+            filename: './packages/template/package.json',
+            type: 'json'
+        }
+    ],
+    skip: {
+        changelog: true
+    },
+    commitAll: true,
+    hooks: {
+        prepublish: 'yarn run build',
+        postpublish: 'lerna publish from-git && yarn pub-template',
+        postreleaseBranch: 'lerna version {{version}} && git add .'
+    }
+};
