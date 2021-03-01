ff5137fc735d8afbe8665b6ba5baae0bde05c5f9
why520crazy <why520crazy@163.com>
2021-03-01 20:07:58 +0800

    fix: fix test error in circleci

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 const env = process.env.NODE_ENV;
+
 if (['development', 'test'].includes(env)) {
-    if (fs.existsSync('./src')) {
+    if (fs.existsSync(path.resolve(__dirname, './src'))) {
         module.exports = require('./src/index');
     } else {
         module.exports = require('./lib/index');
