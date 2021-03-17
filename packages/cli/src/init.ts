6a48c9e9cb3797040161db40b2cb294aebb957a8
chen-yang <wszgrcy@gmail.com>
2021-03-17 14:05:43 +0800

    fix(cli): `docgeni init` default value

--- packages/cli/src/init.ts
+++ packages/cli/src/init.ts
@@ -7,6 +7,7 @@ export const initCommand: CommandModule = {
         yargs.option('mode', {
             alias: 'm',
             desc: `Mode of documentation, full mode contains nav, home page, lite mode only contains menu and doc viewers`,
+            default: 'lite',
             choices: ['lite', 'full']
         });
 
