12da884d5b4d1c6b413112cc38e2ebf591745774
why520crazy <why520crazy@163.com>
2021-03-24 13:31:18 +0800

    build(deps): remove gulp-watch

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,6 @@ module.exports = {
     extends: ['@commitlint/config-angular'],
     rules: {
         'header-max-length': [2, 'always', 120],
-        'scope-enum': [2, 'always', ['cli', 'core', 'template', 'a-lib', 'site', 'toolkit']]
+        'scope-enum': [2, 'always', ['cli', 'core', 'template', 'a-lib', 'site', 'toolkit', 'deps']]
     }
 };
