b0bac84acc98409d8ccef3fa5dcd816b7d191383
chen-yang <wszgrcy@gmail.com>
2021-05-17 17:06:00 +0800

    fix(core): add markdown highlight key `ng`

--- packages/core/src/markdown/markdown.spec.ts
+++ packages/core/src/markdown/markdown.spec.ts
@@ -1,8 +1,9 @@
 import { Markdown } from './markdown';
-describe('markdown', () => {
+fdescribe('markdown', () => {
     const data = ['```bash', 'ng add alib', '```'].join('\n');
     it('run', () => {
         const result = Markdown.toHTML(data);
         expect(result).not.toContain('%20');
+        expect(result).toContain(`<span class="token function">ng</span>`);
     });
 });
