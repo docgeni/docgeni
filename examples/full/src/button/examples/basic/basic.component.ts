2302ab14ca14e61d168ff2d210ea86ab68b5ba52
why520crazy <why520crazy@163.com>
2021-05-18 09:09:43 +0800

    docs: add full and lite examples

---
+++ examples/full/src/button/examples/basic/basic.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-basic',
+    templateUrl: './basic.component.html',
+    styleUrls: ['./basic.component.css']
+})
+export class AlibButtonBasicExampleComponent {
+    constructor() {}
+}
