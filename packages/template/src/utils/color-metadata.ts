82a527575cc736e5e6708e925f82d55da71c0978
wszgrcy <wszgrcy@gmail.com>
2021-08-27 15:31:41 +0800

    fix(template): function name

--- packages/template/src/utils/color-metadata.ts
+++ packages/template/src/utils/color-metadata.ts
@@ -1,23 +1,23 @@
 export function colorMetadata(color: string) {
     let obj: { r: number; g: number; b: number };
     if (color.startsWith('#')) {
-        obj = HexColorMetadata(color);
+        obj = hexColorMetadata(color);
     } else if (color.startsWith('rgb')) {
-        obj = RgbColorMetadata(color);
+        obj = rgbColorMetadata(color);
     } else {
         throw new Error('unsupported color format ' + color);
     }
     return obj;
 }
 
-function HexColorMetadata(color: string) {
+function hexColorMetadata(color: string) {
     const rgx = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
     const hex = color.replace(rgx, (m: any, r: any, g: any, b: any) => r + r + g + g + b + b);
     const rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return { r: parseInt(rgb[1], 16), g: parseInt(rgb[2], 16), b: parseInt(rgb[3], 16) };
 }
 
-function RgbColorMetadata(color: string) {
+function rgbColorMetadata(color: string) {
     const result = /rgb\(([0-9]{1,3})\s*[,\s]\s*([0-9]{1,3})\s*[,\s]\s*([0-9]{1,3})\)/i.exec(color);
     return {
         r: parseInt(result[1], 10),
