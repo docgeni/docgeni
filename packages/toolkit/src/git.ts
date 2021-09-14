d98f774b60e7c6e5b73a794a65cbb2b35f92faef
wszgrcy <wszgrcy@gmail.com>
2021-09-14 17:05:07 +0800

    fix(toolkit): git log `names with spaces` error

--- packages/toolkit/src/git.ts
+++ packages/toolkit/src/git.ts
@@ -1,16 +1,21 @@
 import * as shell from './shell';
 
 export function lastUpdatedTime(filePath: string): number {
-    return parseInt(shell.execSync(`git log --pretty=format:"%at" -n 1 ${filePath}`, { stdio: 'pipe' }).toString(), 10);
+    return parseInt(shell.execSync(`git log --pretty=format:"%at" -n 1 "${filePath}"`, { stdio: 'pipe' }).toString(), 10);
 }
 
 export function contributors(filePath: string | string[]) {
     return Array.from(
         new Set(
             shell
-                .execSync(`git log --pretty=format:"%an" ${typeof filePath === 'string' ? filePath : filePath.join(' ')}`, {
-                    stdio: 'pipe'
-                })
+                .execSync(
+                    `git log --pretty=format:"%an" ${
+                        typeof filePath === 'string' ? '"' + filePath + '"' : filePath.map(item => '"' + item + '"').join(' ')
+                    }`,
+                    {
+                        stdio: 'pipe'
+                    }
+                )
                 .toString()
                 .split('\n')
         )
