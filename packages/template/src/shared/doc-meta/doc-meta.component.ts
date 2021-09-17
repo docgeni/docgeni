8dcb876afe4c26f7641114909451540a84ac76dc
chen <wszgrcy@gmail.com>
2021-09-17 15:02:23 +0800

    feat(template): doc-meta contributor add title

--- packages/template/src/shared/doc-meta/doc-meta.component.ts
+++ packages/template/src/shared/doc-meta/doc-meta.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit, HostBinding } from '@angular/core';
 import { DocItem } from '@docgeni/template/interfaces';
 import { GlobalContext } from '@docgeni/template/services/public-api';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'dg-doc-meta',
@@ -20,6 +21,7 @@ export class DocMetaComponent implements OnInit {
                 .get(`https://api.github.com/repos/${this.globalContext.owner}/${this.globalContext.repo}/commits`, {
                     params: { path: this.docItem.originPath }
                 })
+                .pipe(filter((result: any[]) => !!result.length))
                 .subscribe((result: { author: { avatar_url: string; login: string }; commit: { author: { date: string } } }[]) => {
                     this.contributors = Array.from(new Set(result.map(item => item.author.login)));
                     this.lastUpdatedTime = new Date(result[0].commit.author.date);
