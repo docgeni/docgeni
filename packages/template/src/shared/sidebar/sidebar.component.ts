91402c9639201c590c940e807bf69320cb5d636f
wszgrcy <wszgrcy@gmail.com>
2021-09-24 11:46:02 +0800

    feat(template): sidebar support open and close

--- packages/template/src/shared/sidebar/sidebar.component.ts
+++ packages/template/src/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, HostBinding, Input } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { NavigationItem } from '../../interfaces/public-api';
 import { GlobalContext } from '../../services/global-context';
 
@@ -10,8 +12,47 @@ export class SidebarComponent implements OnInit {
     @HostBinding(`class.dg-sidebar`) isSidebar = true;
 
     @Input() menus: NavigationItem[];
+    menuDisplayMap = new Map<NavigationItem, boolean>();
+    constructor(public global: GlobalContext, private router: Router, private activatedRoute: ActivatedRoute) {}
 
-    constructor(public global: GlobalContext) {}
+    ngOnInit(): void {
+        this.router.events.pipe(filter(item => item instanceof NavigationEnd)).subscribe(() => {
+            this.openToRouter();
+        });
+    }
 
-    ngOnInit(): void {}
+    toggle(menu: NavigationItem) {
+        if (!menu.items || !menu.items.length) {
+            return;
+        }
+        let status = this.menuDisplayMap.get(menu);
+        this.menuDisplayMap.set(menu, !status);
+    }
+
+    ngOnChanges(): void {
+        this.openToRouter();
+    }
+
+    private openToRouter() {
+        this.findRouter(this.menus).forEach(menu => {
+            this.toggle(menu);
+        });
+    }
+
+    private findRouter(menus: NavigationItem[]): NavigationItem[] {
+        for (const menu of menus) {
+            let urlTree = this.router.createUrlTree(['./' + menu.path], { relativeTo: this.activatedRoute });
+            let result = this.router.isActive(urlTree, !menu.examples);
+            if (result) {
+                return [menu];
+            }
+            if (menu.items && menu.items.length) {
+                let result = this.findRouter(menu.items);
+                if (result && result.length) {
+                    return [menu, ...result];
+                }
+            }
+        }
+        return [];
+    }
 }
