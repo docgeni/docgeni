9ec930020d534b2c9a30682a7db03076ff973d73
luxiaobei <luxiaobei@worktile.com>
2021-10-18 18:58:02 +0800

    feat(core): generate sitemap by config #OSP-192 (#263)
    * feat(core): add generate sitemap plugin #OSP-192
    
    * test(core): fix test about site-plugin
    
    * feat(core): build sitemap from docItems & add sitemap test

---
+++ packages/core/src/plugins/sitemap.spec.ts
@@ -0,0 +1,41 @@
+import { DocgeniContext } from '../docgeni.interface';
+import { assertExpectedFiles, createTestDocgeniContext, DEFAULT_TEST_ROOT_PATH, FixtureResult, loadFixture } from '../testing';
+import { toolkit } from '@docgeni/toolkit';
+import * as systemPath from 'path';
+import SitemapPlugin from './sitemap';
+
+const DEFAULT_SITE_PATH = `${DEFAULT_TEST_ROOT_PATH}/.docgeni/site`;
+
+describe('#sitemap-plugin', () => {
+    let context: DocgeniContext;
+    let fixture: FixtureResult;
+    let sitemapPlugin: SitemapPlugin;
+
+    beforeAll(async () => {
+        toolkit.initialize({
+            baseDir: systemPath.resolve(__dirname, '../')
+        });
+        fixture = await loadFixture('default-site');
+    });
+
+    beforeEach(() => {
+        context = createTestDocgeniContext({
+            initialFiles: {}
+        });
+        sitemapPlugin = new SitemapPlugin();
+        sitemapPlugin.apply(context);
+    });
+
+    it('should create sitemap.xml success', async () => {
+        await context.hooks.navsEmitSucceed.call(null, {
+            'en-us': [{ path: 'front-matter', channelPath: 'configuration', id: 'front-matter', title: 'front-matter' }]
+        });
+        await assertExpectedFiles(
+            context.host,
+            {
+                [`${DEFAULT_SITE_PATH}/src/sitemap.xml`]: fixture.src['sitemap.xml']
+            },
+            true
+        );
+    });
+});
