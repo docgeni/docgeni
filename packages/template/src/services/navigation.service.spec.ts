3c9ea1a245bd1589d0efd6fb1c8662169dfba7ab
why520crazy <why520crazy@163.com>
2022-04-21 17:17:26 +0800

    fix(template): get corrected doc item when component is same in multiple  libraries #347

--- packages/template/src/services/navigation.service.spec.ts
+++ packages/template/src/services/navigation.service.spec.ts
@@ -87,6 +87,13 @@ const mockGlobalContext = {
                     ]
                 }
             ]
+        },
+        {
+            id: 'b-components',
+            path: 'b-components',
+            title: 'B Components',
+            lib: 'blib',
+            items: []
         }
     ],
     docItems: [
@@ -109,7 +116,7 @@ const mockGlobalContext = {
         {
             id: 'global',
             path: 'global',
-            channel_path: 'configuration',
+            channelPath: 'configuration',
             title: '全局配置',
             order: 30,
             contentPath: 'docs/configuration/global.html'
@@ -120,6 +127,7 @@ const mockGlobalContext = {
             subtitle: '按钮',
             path: 'button',
             importSpecifier: 'alib/button',
+            channelPath: 'components',
             examples: ['alib-button-basic-example', 'alib-button-advance-title-example', 'alib-button-advance-example'],
             overview: true,
             api: true,
@@ -132,6 +140,7 @@ const mockGlobalContext = {
             subtitle: '测试',
             path: 'foo',
             importSpecifier: 'alib/foo',
+            channelPath: 'components',
             examples: ['alib-foo-advance-example', 'alib-foo-basic-example'],
             overview: true,
             api: false,
@@ -247,6 +256,7 @@ describe('NavigationService', () => {
             subtitle: '按钮',
             path: 'button',
             importSpecifier: 'alib/button',
+            channelPath: 'components',
             examples: ['alib-button-basic-example', 'alib-button-advance-title-example', 'alib-button-advance-example'],
             overview: true,
             api: true,
@@ -255,5 +265,51 @@ describe('NavigationService', () => {
         });
 
         mockGlobalContext.docItems = originalDocItems;
+    });
+
+    it('should get doc item when same name in diff lib', () => {
+        mockGlobalContext.docItems = [
+            {
+                id: 'button',
+                title: 'Button',
+                subtitle: '按钮',
+                path: 'button',
+                importSpecifier: 'b-lib/button',
+                channelPath: 'b-components',
+                examples: [],
+                overview: true,
+                api: true,
+                order: 1,
+                category: 'general'
+            },
+            ...mockGlobalContext.docItems
+        ];
+        spectator.service.selectChannelByPath('components');
+        const docItem = spectator.service.getDocItemByPath('button');
+
+        expect(docItem).toEqual(
+            jasmine.objectContaining({
+                id: 'button',
+                title: 'Button',
+                subtitle: '按钮',
+                path: 'button',
+                importSpecifier: 'alib/button',
+                channelPath: 'components'
+            })
+        );
+
+        spectator.service.selectChannelByPath('b-components');
+        const bDocItem = spectator.service.getDocItemByPath('button');
+
+        expect(bDocItem).toEqual(
+            jasmine.objectContaining({
+                id: 'button',
+                title: 'Button',
+                subtitle: '按钮',
+                path: 'button',
+                importSpecifier: 'b-lib/button',
+                channelPath: 'b-components'
+            })
+        );
     });
 });
