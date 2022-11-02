017a3be8b963936f58411209aa6116a1659561ff
why520crazy <why520crazy@163.com>
2022-11-02 15:54:09 +0800

    fix(core): combine metadata bootstrap as long as one has a value

--- packages/core/src/ast-utils.spec.ts
+++ packages/core/src/ast-utils.spec.ts
@@ -15,55 +15,6 @@ describe('#ast-utils', () => {
     };
         `;
 
-    // describe('generateComponentsModule', () => {
-    //     const moduleMetadata: NgModuleMetadata = {
-    //         imports: ['CommonModule'],
-    //         declarations: ['AppComponent'],
-    //         providers: ['AppService']
-    //     };
-
-    //     const moduleMetadataArgs = Object.keys(moduleMetadata)
-    //         .map(key => {
-    //             return `${key}: [ ${moduleMetadata[key].join(', ')} ]`;
-    //         })
-    //         .join(',\n    ');
-    //     const ngModuleName = 'MyButtonExamplesModule';
-
-    //     const moduleText = `
-    //     @NgModule({
-    //         ${moduleMetadataArgs}
-    //     })
-    //     export class ${ngModuleName} {}
-    //     `;
-
-    //     function expectOriginalSource(output: string) {
-    //         expect(output).toMatch(`import { AlibComponent } from './basic.component';`);
-    //         expect(output).toMatch(`export class MyButtonExamplesModule {}`);
-    //         expect(output).toMatch(/declarations: \[ AppComponent \]/);
-    //         expect(output).toMatch(/providers: \[ AppService \]/);
-    //         expect(output).toMatch(/imports: \[ CommonModule \],/);
-    //     }
-
-    //     it('should generate module success with default export', () => {
-    //         const ngSourceFile = createNgSourceFile('module.ts', sourceText);
-
-    //         const components = [{ name: 'AlibComponent', moduleSpecifier: './basic.component' }];
-    //         const output = utils.generateComponentsModule(ngSourceFile, moduleText, components);
-
-    //         expectOriginalSource(output);
-    //         expect(output).toMatch(`import { CommonModule } from '@angular/common';`);
-    //         expect(output).toMatch(`import { AppComponent } from './app.component';`);
-    //     });
-
-    //     it('should generate module success with empty', async () => {
-    //         const ngSourceFile = createNgSourceFile('module.ts', '');
-    //         const components = [{ name: 'AlibComponent', moduleSpecifier: './basic.component' }];
-    //         const output = await utils.generateComponentsModule(ngSourceFile, moduleText, components);
-
-    //         expectOriginalSource(output);
-    //     });
-    // });
-
     describe('insertImports', () => {
         it('should return changes when sourceFile has import and importStructures is []', () => {
             const ngSourceFile = createNgSourceFile('module.ts', sourceText);
@@ -165,7 +116,7 @@ describe('#ast-utils', () => {
     describe('combineNgModuleMetadata', () => {
         it('when metadata and append has value', () => {
             const combinedMetadata = utils.combineNgModuleMetadata(
-                { imports: ['FormsModule'], providers: ['AppService'] },
+                { imports: ['FormsModule'], providers: ['AppService'], bootstrap: ['RootComponent'] },
                 { imports: ['CommonModule'], declarations: ['AppComponent'] }
             );
             expect(combinedMetadata).toEqual({
@@ -173,7 +124,8 @@ describe('#ast-utils', () => {
                 entryComponents: [],
                 providers: ['AppService'],
                 imports: ['FormsModule', 'CommonModule'],
-                exports: []
+                exports: [],
+                bootstrap: ['RootComponent']
             });
         });
 
@@ -222,6 +174,22 @@ describe('#ast-utils', () => {
                 imports: [],
                 exports: []
             });
+        });
+
+        it('should get undefined when bootstrap metadata is empty', () => {
+            const combinedMetadata = utils.combineNgModuleMetadata(
+                { imports: ['FormsModule'], providers: ['AppService'] },
+                { imports: ['CommonModule'], declarations: ['AppComponent'] }
+            );
+            expect(combinedMetadata.bootstrap).toEqual(undefined);
+        });
+
+        it('should get bootstrap when append bootstrap metadata has value', () => {
+            const combinedMetadata = utils.combineNgModuleMetadata(
+                { imports: ['FormsModule'], providers: ['AppService'] },
+                { imports: ['CommonModule'], declarations: ['AppComponent'], bootstrap: ['RootComponent'] }
+            );
+            expect(combinedMetadata.bootstrap).toEqual(['RootComponent']);
         });
     });
 
