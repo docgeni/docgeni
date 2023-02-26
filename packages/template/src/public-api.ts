7b746268af38f7ab095c6cb09e560fec9d02a5a9
Guoxiin <guoxiin@163.com>
2023-02-27 02:49:08 +0800

    build: update to ng13

--- packages/template/src/public-api.ts
+++ packages/template/src/public-api.ts
@@ -2,14 +2,16 @@
  * Public API Surface of @docgeni/template
  */
 
-export * from './module';
-export * from './shared/shared.module';
-export * from './pages/pages.module';
-export * from './services/public-api';
+export { addBuiltInComponents, BuiltInComponentDef, DocgeniBuiltInComponent } from './built-in';
 export * from './built-in/index';
+export * from './initializer';
+export * from './interfaces/public-api';
+export * from './module';
+export * from './pages/pages.module';
+export * from './pages/public-api';
+export * from './routes';
+export * from './services/public-api';
 // It will warn Circular dependence when export from /services/public-api
 export * from './services/router-reset.service';
-export * from './routes';
-export * from './interfaces/public-api';
-export * from './initializer';
-export { DocgeniBuiltInComponent, addBuiltInComponents, BuiltInComponentDef } from './built-in';
+export * from './shared/public-api';
+export * from './shared/shared.module';
