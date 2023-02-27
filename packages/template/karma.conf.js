afa369d1643c0fc4f055596c12fb2f7db8fa51ab
Guoxiin <guoxiin@163.com>
2023-02-27 11:55:41 +0800

    fix: ng run template error

--- packages/template/karma.conf.js
+++ packages/template/karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function(config) {
             require('karma-chrome-launcher'),
             require('karma-jasmine-html-reporter'),
             require('karma-coverage-istanbul-reporter'),
-            require('@angular-devkit/build-angular/plugins/karma')
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-coverage')
         ],
         client: {
             clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -33,6 +34,7 @@ module.exports = function(config) {
                 base: 'ChromeHeadless',
                 flags: ['--no-sandbox']
             }
-        }
+        },
+        files: [require('path').join(__dirname, 'polyfills.js')]
     });
 };
