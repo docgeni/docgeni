1401296678705b0abb41a17e05e439fd0529666e
minlovehua <321512939@qq.com>
2023-06-29 21:45:58 +0800

    fix(template): add cdkScrollable to fix examples for components related to cdk overlay #INFR-3715

--- .docgeni/app/module.ts
+++ .docgeni/app/module.ts
@@ -1,7 +1,8 @@
 import { FormsModule } from '@angular/forms';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { myProviders } from './providers';
 
 export default {
-    imports: [FormsModule],
+    imports: [FormsModule, ScrollingModule],
     providers: [...myProviders]
 };
--- packages/template/src/pages/channel/channel.component.ts
+++ packages/template/src/pages/channel/channel.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit, OnDestroy, HostBinding, NgModuleFactory, Type, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding, NgModuleFactory, Type, ElementRef, NgZone } from '@angular/core';
+import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NavigationService, GlobalContext } from '../../services/public-api';
 
 @Component({
     selector: 'dg-channel',
-    templateUrl: './channel.component.html'
+    templateUrl: './channel.component.html',
+    providers: [
+        {
+            provide: CdkScrollable,
+            useExisting: ChannelComponent
+        }
+    ]
 })
-export class ChannelComponent implements OnInit, OnDestroy {
+export class ChannelComponent extends CdkScrollable implements OnInit, OnDestroy {
     @HostBinding(`class.dg-layout`) isLayout = true;
     @HostBinding(`class.dg-scroll-container`) isScrollContainer = true;
 
@@ -17,15 +24,19 @@ export class ChannelComponent implements OnInit, OnDestroy {
     exampleModuleFactory: NgModuleFactory<any> | null = null;
 
     constructor(
-        private http: HttpClient,
-        private elementRef: ElementRef<HTMLElement>,
         private route: ActivatedRoute,
-        private router: Router,
+        public elementRef: ElementRef<HTMLElement>,
+        public scrollDispatcher: ScrollDispatcher,
+        public ngZone: NgZone,
         public navigationService: NavigationService,
         public global: GlobalContext
-    ) {}
+    ) {
+        super(elementRef, scrollDispatcher, ngZone);
+    }
 
     ngOnInit(): void {
+        super.ngOnInit();
+
         const path = this.route.snapshot.routeConfig?.path!;
         this.navigationService.selectChannelByPath(path);
     }
