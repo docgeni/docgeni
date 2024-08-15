02772695106588237d2688172231d0131a1ccdca
why520crazy <why520crazy@163.com>
2024-08-15 22:00:19 +0800

    refactor: use fs-extra copy instead of read/write

--- packages/toolkit/src/testing/fs.ts
+++ packages/toolkit/src/testing/fs.ts
@@ -1,5 +1,14 @@
 import { virtualFs } from '@angular-devkit/core';
 import { DocgeniFsHostImpl } from '../fs';
+import { normalize } from '../path';
+import { lastValueFrom } from 'rxjs';
+
+class TestDocgeniFsHostImpl extends DocgeniFsHostImpl {
+    async copyFile(src: string, dest: string): Promise<void> {
+        const data = await lastValueFrom(this.host.read(normalize(src)));
+        await lastValueFrom(this.host.write(normalize(dest), data));
+    }
+}
 
 export function createTestFsHost(files: Record<string, string>) {
     return new virtualFs.test.TestHost(files);
@@ -10,5 +19,5 @@ export function createTestFsSyncDelegateHost(files: Record<string, string>) {
 }
 
 export function createTestDocgeniFsHost(initialFiles: Record<string, string> = {}) {
-    return new DocgeniFsHostImpl(new virtualFs.test.TestHost(initialFiles));
+    return new TestDocgeniFsHostImpl(new virtualFs.test.TestHost(initialFiles));
 }
