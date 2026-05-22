b3be106894edc8422ee0ddad5d797e368d13a5f7
why520crazy <why520crazy@163.com>
2026-05-22 22:57:20 +0800

    feat: add tabs built-in component

--- packages/template/src/built-in/built-in.module.ts
+++ packages/template/src/built-in/built-in.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DocgeniLabelComponent } from './label/label.component';
 import { DocgeniAlertComponent } from './alert/alert.component';
+import { DocgeniTabsComponent } from './tabs/tabs.component';
 import { loadBuiltInComponents } from './loader';
 
 @NgModule({
-    declarations: [DocgeniLabelComponent, DocgeniAlertComponent],
+    declarations: [DocgeniLabelComponent, DocgeniAlertComponent, DocgeniTabsComponent],
     imports: [CommonModule],
     exports: [],
     providers: [],
--- packages/template/src/built-in/loader.ts
+++ packages/template/src/built-in/loader.ts
@@ -1,7 +1,8 @@
 import { setBuiltInComponents } from './built-in-components';
 import label from './label/label.component';
 import alert from './alert/alert.component';
+import tabs from './tabs/tabs.component';
 
 export function loadBuiltInComponents() {
-    setBuiltInComponents([label, alert]);
+    setBuiltInComponents([label, alert, tabs]);
 }
